9809186cf4f13f6153a1821bfe3a8331fda17952
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-09-06 14:59:07 +0600

    finish up scrolling

--- components/Navbar.js
+++ components/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
         <div className='mx-auto hidden max-w-3xl items-center justify-center rounded-lg border border-stone-50/30 bg-black/30 py-3 backdrop-blur-lg lg:flex'>
           <div className='flex items-center justify-between gap-6'>
             <div>
-              <Link href='#home' className='font-bold text-2xl'>
+              <Link href='#home' onClick={(e)=> handleLinkClick(e, item.href)} className='font-bold text-2xl'>
                 Mahfujul Sagor
               </Link>
             </div>
@@ -57,7 +57,7 @@ const Navbar = () => {
         <div className='rounded-lg lg:hidden max-w-3xl py-3 px-4 mx-auto border border-stone-50/30 bg-black/30 backdrop-blur-lg'>
           <div className='flex items-center justify-between'>
             <div>
-              <Link href='#home' className='font-bold text-xl sm:text-2xl'>
+              <Link href='#home' onClick={(e)=> handleLinkClick(e, item.href)} className='font-bold text-xl sm:text-2xl'>
                 Mahfujul Sagor
               </Link>
             </div>
